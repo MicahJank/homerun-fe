46a44867f847c1c0222907f4c2c848c849cb024d
MicahJank <MicahJank@users.noreply.github.com>
2020-07-16 11:02:33 -0500

    added some segments to the account setting page

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Row, Col } from "antd";
+import { Segment } from 'semantic-ui-react';
 import ChildAccountDropdown from "./ChildAccountDropdown";
 
 const Account = () => {
   return (
-    <div className="account container">
-      <Row style={{ margin: "50px 0" }}>
-        <Col span={24}>
-
-        </Col>
-      </Row>
-    </div>
+    <Segment.Group size="huge">
+      <Segment placeholder>
+        
+      </Segment>
+      <Segment placeholder></Segment>
+    </Segment.Group>
   );
 };
 
