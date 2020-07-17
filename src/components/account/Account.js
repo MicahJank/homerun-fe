9cda9ce0454d859e4d67cee2464c74e6f45e2890
MicahJank <MicahJank@users.noreply.github.com>
2020-07-17 12:32:15 -0500

    update user account page

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,15 +1,56 @@
 import React from "react";
 import { Row, Col } from "antd";
-import { Segment } from 'semantic-ui-react';
+import { Segment, Image, Icon, Button, Label, Header } from 'semantic-ui-react';
 import ChildAccountDropdown from "./ChildAccountDropdown";
 
 const Account = () => {
   return (
     <Segment.Group size="huge">
-      <Segment placeholder>
-        
+      <Segment size="mini" >
+        <div className="avatar-section">
+          <div className="user-profile-img">
+            <Icon color="orange" name="user" size='big' />
+          </div>
+          <Button color="orange" className="avatar-upload-btn" size="big" animated >
+            <Button.Content visible>Upload Image</Button.Content>
+            <Button.Content hidden><Icon size="large" name="upload" /></Button.Content>
+          </Button>
+        </div>
       </Segment>
-      <Segment placeholder></Segment>
+
+      <Header as="h4" dividing>
+        <Header.Content>
+        NAME
+        </Header.Content>
+        <Header.Content>
+        NAME
+        </Header.Content>
+        <Header.Content>
+        NAME
+        </Header.Content>
+      </Header>
+      <Header as="h4" dividing>
+        <Header.Content>
+          EMAIL
+        </Header.Content>
+        <Header.Content>
+          EMAIL
+        </Header.Content>
+        <Header.Content>
+          EMAIL
+        </Header.Content>
+      </Header>
+      <Header as="h4" dividing>
+        <Header.Content>
+          PASSWORD
+        </Header.Content>
+        <Header.Content>
+          PASSWORD
+        </Header.Content>
+        <Header.Content>
+          PASSWORD
+        </Header.Content>
+      </Header>
     </Segment.Group>
   );
 };
