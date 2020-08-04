4d0a9eca4986f4272265741659fee42bdadc57ea
MicahJank <MicahJank@users.noreply.github.com>
2020-08-04 13:43:11 -0500

    working on error handling in account settings

--- src/components/account/UpdateEmail.js
+++ src/components/account/UpdateEmail.js
@@ -10,26 +10,50 @@ const UpdateEmail = ({ info, setModalOpen }) => {
     const [email, setEmail] = useState('');
     const loading = useSelector(state => state.user.loading);
     const userInfo = useSelector(state => state.user.userInfo);
+    const [errorHidden, setErrorHidden] = useState(true);
+    const [inputError, setInputError] = useState(false);
+    const [errorInfo, setErrorInfo] = useState("");
 
 
     const handleChange = (e, data) => {
+        setInputError(false)
         setEmail(data.value)
     }
     
     const handleUpdate = () => {
+        // check the data the user is submitting to see if it is in valid email format
+        if (!email) {
+            setErrorInfo("Invalid email. Email field cannot be blank.")
+            setErrorHidden(false);
+            setInputError(true);
+            return
+        }
+        if (!email.includes('@')) {
+            setErrorInfo("Invalid email. Missing '@'.")
+            setErrorHidden(false);
+            setInputError(true);
+            return
+        }
         axiosWithAuth().put('/members/update-email', { email, memberId: userInfo.member_id })
             .then(res => {
-                alert("Email has ben sent")
+                setInputError(false);
+                setErrorInfo('')
+                setErrorHidden(true)
+                alert("Email has ben sent") // <- should be alerted in the account page section not the modal section
             })
             .catch(err => {
-                console.log(err);
+                setInputError(true);
+                setErrorInfo(err.response.data.message)
+                setErrorHidden(false)
+                console.log(err.response);
             })
     }
 
     return (
         <>
         <Message>Please enter a new email for your account below</Message>
-        <Input onChange={handleChange} size="large" placeholder={info} />
+        <Message error hidden={errorHidden}>{errorInfo}</Message>
+        <Input error={inputError} onChange={handleChange} type="email" size="large" placeholder={info} />
         <div className="btns-section">
             <Button onClick={handleUpdate} floated='right' primary>Update Email</Button>
             <Button onClick={() => setModalOpen(false)} floated='right' negative>Cancel</Button>
--- src/components/account/UpdatePassword.js
+++ src/components/account/UpdatePassword.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input, Message, Button, Form } from 'semantic-ui-react';
-
+import { useSelector, useDispatch } from 'react-redux';
+import actions from '../../actions';
+import axiosWithAuth from '../../utils/AxiosWithAuth';
 
 
 const UpdatePassword = ({ info, setModalOpen }) => {
-// TODO backend is ready to go mostly - just need to get the front end hitting the backend api and updating the password
+    const memberId = useSelector(state => state.user.userInfo.member_id);
+    const [passwordInfo, setPasswordInfo] = useState({ oldPassword: '', newPassword: '', memberId });
+    const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+
+    const handleChange = (e, data) => {
+        setPasswordInfo({...passwordInfo, [data.name]: data.value })
+    }
+    
+    const handleUpdate = () => {
+        console.log(passwordInfo)
+        setLoading(true)
+        axiosWithAuth().put('/auth/update-password', passwordInfo)
+            .then(res => {
+                alert("Password has been updated")
+                console.log(res);
+                setPasswordInfo({ oldPassword: '', newPassword: '', memberId })
+                setLoading(false)
+                setModalOpen(false)
+            })
+            .catch(err => {
+                setLoading(false)
+                console.log(err);
+            })
+    }
+
+
     return (
         <>
         <Message>Please enter a new password for your account below</Message>
-        <Form size={"large"}>
-            <Form.Input type="password" required placeholder="Current Password" />
-            <Form.Input type="password" required placeholder="New Password" />
+        <Form loading={loading} size={"large"}>
+            <Form.Input onChange={handleChange} name="oldPassword" type="password" required placeholder="Current Password" />
+            <Form.Input onChange={handleChange} name="newPassword" type="password" required placeholder="New Password" />
         </Form>
         <div className="btns-section">
-            <Button floated='right' primary>Update Password</Button>
+            <Button loading={loading} onClick={handleUpdate} floated='right' primary>Update Password</Button>
             <Button onClick={() => setModalOpen(false)} floated='right' negative>Cancel</Button>
         </div>
         </>
